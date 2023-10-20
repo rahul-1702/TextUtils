b49c5353fb097eaa9e9584e63b07bdb4a67bc353
Rahul-taak <rtaak786@gmail.com>
2023-10-20 23:52:47 +0530

    first text

---
+++ src/components/Navbar.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import TextForm from "./TextForm";
+
+export default function Navbar() {
+  const [myStyle, setStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  const toggleColor = () => {
+    if (myStyle.backgroundColor === "white") {
+      document.title = "Text Utils - Dark Mode";
+      setStyle({
+        color: "white",
+        backgroundColor: "#001922",
+      });
+    } else {
+      document.title = "Text Utils - Light Mode";
+      setStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+    }
+  };
+
+    if (myStyle.backgroundColor === "white") {
+      var myMode = {
+        color: "black",
+        cursor: "pointer"
+      };
+      var navCol = {
+        color: "black",
+        backgroundColor: "#dbdbdb",
+        fontSize: '17px'
+      }
+    } else {
+      myMode = {
+        color: "white",
+        cursor: "pointer"
+      };
+      navCol = {
+        Color: "white !important",
+        backgroundColor: "#001922",
+        fontSize: '17px'
+      }
+    }
+
+  return (
+    <>
+      <nav className={`navbar navbar-expand-lg p-3`} style={navCol}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/" style={navCol}>
+            <b>Text Utils App</b>
+          </a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              {/* <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/" style={navCol}>
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/" style={navCol}>
+                  About
+                </a>
+              </li> */}
+              <p style={{height: '100%', margin: 'auto 0px', color: 'gray'}}>Created By Rahul Taak</p>
+            </ul>
+          </div>
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input mt-1 me-2"
+              type="checkbox"
+              style={{
+                cursor: "pointer",
+                border: "2px solid grey",
+                width: "40px",
+                height: "20px",
+              }}
+              onClick={toggleColor}
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label align-middle me-1"
+              htmlFor="flexSwitchCheckDefault"
+              style={myMode}
+            >
+              Dark Mode
+            </label>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', textAlign: 'center'}}>
+              <div style={{width: '30px', height: '2px', border: `1px solid gray`, borderRadius: '5px'}}></div>
+              <div style={{width: '30px', height: '2px', border: `1px solid gray`, borderRadius: '5px'}}></div>
+              <div style={{width: '30px', height: '2px', border: `1px solid gray`, borderRadius: '5px'}}></div>
+            </span>
+          </button> 
+        </div>
+      </nav>
+      <div className="container"><TextForm mode={myStyle.color}/></div>
+    </>
+  );
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import './TextForm.css'
+
+export default function TextForm(props) {
+
+  const [text, setText] = useState("");
+
+  const handlerChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const changeUp = () => {
+    setText(text.toUpperCase());
+  };
+
+  const changeLo = () => {
+    setText(text.toLowerCase());
+  };
+
+  const changeCapt = () => {
+    const words = text.split(" ");
+    const capitalizedWords = words.map((word) => {
+      if (word.length > 0) {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+      }
+      return "";
+    });
+    const capitalizedString = capitalizedWords.join(" ");
+    setText(capitalizedString);
+  };
+
+  const copyToClipboard = () => {
+    var text = document.getElementById('mybox');
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const resetAll = () => {
+    setText("");
+  };
+
+  const removeSpace = () => {
+    // Remove extra spaces and update the cleanedText state
+    const trimmedText = text.replace(/\s+/g, " ").trim();
+    setText(trimmedText);
+  };
+
+  if(props.mode === 'white'){
+    var textAreaStyle = {
+      backgroundColor: '#001922',
+      color: 'white',
+      height: '400px'
+    }
+    var word = {
+      color: "white",
+      textAlign: "right"
+    };
+    var heading = {
+      color: "white"
+    };
+    document.getElementsByTagName('body')[0].style.backgroundColor = "#022b3a";
+  }
+  else{
+    textAreaStyle = {
+      backgroundColor: 'white',
+      color: 'black',
+      height: '400px'
+    }
+    word = {
+      color: "black",
+      textAlign: "right"
+    };
+    heading = {
+      color: "black"
+    };
+    document.getElementsByTagName('body')[0].style.backgroundColor = "#ffffff";
+  }
+
+  return (
+    <>
+      <h2 className="my-3" style={heading}>
+        TEXT UTILS - A Better Way To Modify And Analyze Your Text
+      </h2>
+      <hr style={heading}/>
+      <div
+        style={{
+          border: `1px solid ${props.mode === 'white'?"white":"gray"}`,
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <button className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`} onClick={changeUp}>
+          UpperCase
+        </button>
+        <button className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`} onClick={changeLo}>
+          LowerCase
+        </button>
+        <button
+          className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`}
+          onClick={changeCapt}
+        >
+          Capitalize
+        </button>
+        <button
+          className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`}
+          onClick={removeSpace}
+        >
+          RemoveSpace
+        </button>
+        <button
+          className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`}
+          onClick={copyToClipboard}
+        >
+          Copy To Clipboard
+        </button>
+        <button className={`btn btn-${props.mode === 'white'?"light":"dark"} btn-sm my-1 me-2`} onClick={resetAll}>
+          Reset All
+        </button>
+        <div className="form my-2">
+          <textarea
+            className="form-control"
+            value={text}
+            id="mybox"
+            onChange={handlerChange}
+            placeholder="Enter Your Text Here..."
+            style={textAreaStyle}
+          ></textarea>
+        </div>
+        <div style={word}>
+          Words : <b>{text.split(" ").length - 1}</b> &nbsp;|&nbsp; Characters :{" "}
+          <b>{text.length}</b>
+        </div>
+      </div>
+      <div className="container" style={heading}>
+        <br/>
+        <h6>Text Utils - Copyright &copy; {new Date().getFullYear()} | All Rights Reserved</h6>
+        <p style={{fontSize: '15px'}}>Created By <b>Rahul Taak</b></p>
+      </div>
+    </>
+  );
+}
